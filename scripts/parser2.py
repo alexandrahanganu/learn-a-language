3170b8082ed56d9c3458e625f593bb8856abb201
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2021-04-26 20:14:51 +0300

    Found a syntax to parse sentences

--- scripts/parser2.py
+++ scripts/parser2.py
@@ -1,15 +1,13 @@
 import spacy
-
-
 def read_file(file_name):
     with open(file_name, "r") as fin:
         text = fin.read()
     return text
 
 
-def tokenize_text():
+def tokenize_text(text):
     sp = spacy.load('en_core_web_sm')
-    file = sp(read_file("text.txt"))
+    file = sp(text)
     return file
 
 
@@ -18,13 +16,26 @@ def tag_text(file):
     tag = []
     exp = []
     for i in range(0, len(file)):
-        print(file[i].pos_)
+        #print(file[i].pos_)
         pos.append(file[i].pos_)
         tag.append(file[i].tag_)
         exp.append(spacy.explain(file[i].tag_))
-    return pos, tag, exp
+    return tag
 
 
+import os
 if __name__ == '__main__':
-    tokenize_text()
-    print(tag_text(tokenize_text()))
+    text = input("Introduceti textul:")
+    file = tokenize_text(text)
+    tags = tag_text(file)
+    #tokenize_text()
+    #print(tag_text(tokenize_text()))
+    print("tagurile propozitiei", tags)
+    os.system(r'"D:/CLIPS/CLIPSWin.exe"') #aici aveti clips-ul instalat
+    some_text = read_file("C:/Users/Theo/PycharmProjects/Pbr Project/help/aux_file.txt") #path-ul fisierului aux_file (e in folderul help)
+    print(some_text)
+    # intreaba la inceput cate propozitii vrea sa citeasca
+    # daca una, atunci o sa citeasca dintr-un anumit fisier cu reguli singulare
+    # daca mai multe din celalalt
+    #     	(rule G9  B2 . EPS)
+    #       (rule G6  E1 . EPS)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\School\learn-a-language\venv
+set VIRTUAL_ENV=C:\Users\Theo\PycharmProjects\Pbr Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
